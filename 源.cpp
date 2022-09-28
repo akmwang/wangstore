4bf5334135f749220e352e69e3b49a78a34ea5e7
akmwang <114556103+akmwang@users.noreply.github.com>
2022-09-28 20:43:26 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,23 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-	float length, width;
+	const int add = 10;
+	int num;
 
-	printf("Please enter the length, ");
-	printf("and enter q to quit: ");
-	while (scanf_s("%f", &length) == 1)
-	{
-		printf("The length is %.2f.\n", length);
-		printf("Please enter the width, ");
-		printf("and enter q to quit: ");
-		if (scanf_s("%f", &width) != 1)
-			break;
-		printf("The width is %.2f.\n", width);
-		printf("The area is %.2f.\n", length * width);
-		printf("Please enter the length, ");
-		printf("and enter q to quit: ");
-	}
-	printf("Bye!\n");
+	printf("Please input an integer:\n");
+	scanf_s("%d", &num);
+	for (int i = num; i <= num + add; i++)
+		printf("%d ", i);
 
 	return 0;
 }
